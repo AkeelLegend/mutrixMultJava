5f00358de61105ad73afe9d6fb9d1d1007b3a997
Akeel Mohamed <akeelm@sltc.lk>
2018-02-27 03:15:35 +0530

    file write issue in the previous commit was done in this issuegit add . only the arguments checking is remaining

--- src/midsemscassignment/writeArraytoFile.java
+++ src/midsemscassignment/writeArraytoFile.java
@@ -3,23 +3,31 @@ package midsemscassignment;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 /**
  *
  * @author Akeel Muhammed
  */
 public class writeArraytoFile {
     public writeArraytoFile(int[][] a, String filepath) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new FileWriter(filepath));
-        bw.write("your answer is\n");
-        BufferedWriter bw1 = new BufferedWriter(new FileWriter(filepath, true));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(filepath, true));
+        PrintWriter writer = new PrintWriter(bw);
+        //writer.println("Your answer is");
+        //BufferedWriter bw1 = new BufferedWriter(new FileWriter(filepath, true));
         
         for(int i=0; i< a.length; i++){
-            bw1.append('\n');
+            //bw.append('\n');
+            
             for(int j=0; j<a[i].length; j++){
-                bw1.append((char)a[i][j]);
-                bw1.append(',');
+                writer.append(Integer.toString(a[i][j]));
+                if(j<(a[i].length - 1))writer.append(',');
+                //bw.append((char)a[i][j]);
+                //bw.append(',');
             }
+            writer.println();
+            
         }
+        writer.close();
         
     }
 }
